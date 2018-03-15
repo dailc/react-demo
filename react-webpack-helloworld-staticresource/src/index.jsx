0bad8aaf60a3b4f764e40398058572b26fc20366
Lichun Dai <a546684355@qq.com>
2018-03-15 15:21:52 +0800

    增加静态资源处理后的示例

---
+++ react-webpack-helloworld-staticresource/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {
+    render,
+} from 'react-dom';
+import './style/libs/mui/mui.css';
+import AppComponent from './components/tab1';
+
+render(<AppComponent />, document.getElementById('content'));
